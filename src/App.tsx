4c2f2ada3b3025cabc749ffe5465fffb9713fdee
shuby mao <le-li.mao@mail.mcgill.ca>
2021-04-22 17:10:49 -0400

    fixed some eslint problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import Game from './lib/game'
 import ThreeWrapper from './components/ThreeWrapper';
