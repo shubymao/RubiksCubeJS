2d3b46fffc82d30930af7c0203688b559b13f16a
shubymao <shubymao@users.noreply.github.com>
2021-04-22 21:12:11 +0000

    deploy: 4c2f2ada3b3025cabc749ffe5465fffb9713fdee

---
+++ precache-manifest.91b51e51d733689b03d665d3dcd648fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4545c7bc0b1f34cb6f0848784dae467b",
+    "url": "/rubiks-cube-web/index.html"
+  },
+  {
+    "revision": "c3f4be69116e6fcefb58",
+    "url": "/rubiks-cube-web/static/css/main.03151aaa.chunk.css"
+  },
+  {
+    "revision": "fa09b147be2340a5bf3b",
+    "url": "/rubiks-cube-web/static/js/2.c31964f2.chunk.js"
+  },
+  {
+    "revision": "c3f4be69116e6fcefb58",
+    "url": "/rubiks-cube-web/static/js/main.bd770790.chunk.js"
+  },
+  {
+    "revision": "d9bef2eedcf1260eb8a1",
+    "url": "/rubiks-cube-web/static/js/runtime-main.ee509a14.js"
+  }
+]);
